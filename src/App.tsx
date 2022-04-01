dfc30f940a6448804fcb9912042d74543dbf4141
Sammy Teillet <sammy.teillet@gmail.com>
2022-04-01 12:39:58 +0200

    feature: Add topbar nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,47 @@
-import React, { useState } from "react";
-import { MoonIcon } from "./icons/Moon";
-import logo from "./logo.svg";
+import "@pluralsight/ps-design-system-normalize";
 
-function App() {
-  const [count, setCount] = useState(0);
+import React from "react";
 
-  function toggleDarkMode() {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      localStorage.setItem("theme", "light");
-      document.documentElement.classList.remove("dark");
-    } else {
-      localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
-    }
-  }
+import AppFrame from "@pluralsight/ps-design-system-appframe";
+import * as core from "@pluralsight/ps-design-system-core";
+import * as Layout from "@pluralsight/ps-design-system-layout";
+import Theme from "@pluralsight/ps-design-system-theme";
+import { SkillsTopNav } from "./TopNav";
 
+// https://coolors.co/23f0c7-fb6107-f3de2c-5c8001-fbb02d
+
+export default function App() {
   return (
-    <div className="flex flex-col min-h-screen text-center bg-white dark:bg-gray-700">
-      <nav className="flex items-center justify-end h-16 px-4">
-        <button
-          className="flex p-2 text-black rounded hover:text-yellow-400 dark:text-white focus:outline-none hover:bg-gray-700 dark:hover:bg-gray-500 dark:hover:text-yellow-400"
-          onClick={toggleDarkMode}
-        >
-          <MoonIcon></MoonIcon>
-        </button>
-      </nav>
-      <header className="flex flex-col items-center justify-center flex-grow text-2xl text-gray-700 dark:text-white">
-        <img src={logo} className="animate-spin-slow h-72" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p className="my-8">
-          <button
-            className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            Count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="text-blue-400"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="text-blue-400"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Theme name={Theme.names.dark}>
+      <ExampleFrame>
+        <div style={{ background: core.colorsBlack }}>
+          <Layout.PageWidthLayout>
+            <Layout.PageHeadingLayout heading={<h1>Webhooks list</h1>} />
+          </Layout.PageWidthLayout>
+        </div>
+      </ExampleFrame>
+    </Theme>
   );
 }
 
-export default App;
+function ExampleFrame(props: {
+  children:
+    | boolean
+    | React.ReactChild
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+}) {
+  return (
+    <>
+      <AppFrame
+        topnav={() => {
+          return <SkillsTopNav />;
+        }}
+      >
+        {props.children}
+      </AppFrame>
+    </>
+  );
+}
