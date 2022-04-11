55a03a6e689e37bea813e83d2cfcb3f87f1edd7f
Sammy Teillet <sammy.teillet@gmail.com>
2022-04-11 19:07:41 +0200

    chore(): Remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/client/",
   plugins: [reactRefresh(), splitVendorChunkPlugin()],
 });
