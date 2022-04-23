6a8fc08491f849118dc805d1a5327688493f74b4
Sammy Teillet <sammy.teillet@gmail.com>
2022-04-23 11:03:27 +0200

    chore(): Add Honey badger

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Honeybadger from "@honeybadger-io/js";
+import { HoneybadgerErrorBoundary } from "@honeybadger-io/react";
+
+Honeybadger.configure({
+  apiKey: import.meta.env.VITE_HONEYBADGER_API_KEY as string,
+  environment: import.meta.env.MODE,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HoneybadgerErrorBoundary honeybadger={Honeybadger}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HoneybadgerErrorBoundary>
 );
