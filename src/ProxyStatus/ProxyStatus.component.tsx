d5a92ac73fc598d4d674be971c64cef85df067a8
Samox <sammy.teillet@gmail.com>
2022-08-22 19:11:54 +0200

    feature(Proxy): Add animation on help icon

--- src/ProxyStatus/ProxyStatus.component.tsx
+++ src/ProxyStatus/ProxyStatus.component.tsx
@@ -1,11 +1,50 @@
 import { Label } from "@pluralsight/ps-design-system-text";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Pulser } from "./Pulser.component";
-import Icon, { CodeIcon, HelpIcon } from "@pluralsight/ps-design-system-icon";
 import NavItem from "@pluralsight/ps-design-system-navitem";
+import helpIcon from "../animations/help.json";
+import lottie from "lottie-web";
 
 const openProxyCommand = "npx webhook-store-cli --noOpen";
+
+function HelpText() {
+  const container = useRef(null);
+
+  useEffect(() => {
+    if (container.current) {
+      lottie.loadAnimation({
+        container: container.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: false,
+        animationData: helpIcon,
+      });
+    }
+
+    return () => {
+      lottie.destroy();
+    };
+  }, []);
+
+  return (
+    <>
+      <Label
+        onMouseEnter={() => lottie.play()}
+        onMouseLeave={() => lottie.pause()}
+        size={Label.sizes.xSmall}
+        style={{ marginRight: "8px", marginLeft: "8px" }}
+      >
+        Debug webhooks on your localhost
+      </Label>
+      <div
+        ref={container}
+        onMouseEnter={() => lottie.play()}
+        onMouseLeave={() => lottie.pause()}
+      />
+    </>
+  );
+}
 
 export function ProxyStatus() {
   const [status, setStatus] = useState<"unkown" | "OK" | "Offline">("unkown");
@@ -47,17 +86,7 @@ export function ProxyStatus() {
             <Pulser />
           </>
         ) : null}
-        {status === "Offline" ? (
-          <>
-            <Label
-              size={Label.sizes.xSmall}
-              style={{ marginRight: "8px", marginLeft: "8px" }}
-            >
-              Debug webhooks on your localhost
-            </Label>
-            <HelpIcon size={Icon.sizes.xSmall} />
-          </>
-        ) : null}
+        {status === "Offline" ? <HelpText /> : null}
       </div>
     </NavItem>
   );
