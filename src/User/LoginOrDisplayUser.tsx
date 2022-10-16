db640543fd05539c97efe19e9dadf1c72a6829a9
Samox <sammy.teillet@gmail.com>
2022-10-16 16:48:36 +0200

    refactor(): Rename accessToken in identityToken

--- src/User/LoginOrDisplayUser.tsx
+++ src/User/LoginOrDisplayUser.tsx
@@ -2,32 +2,32 @@ import React, { useCallback } from "react";
 import NavUser from "@pluralsight/ps-design-system-navuser";
 import { decodeJWT } from "../utils/decode-jwt";
 
-const getAccessTokenFromStorageAndCleanUrl = (): string | null => {
-  const storedAccessToken = localStorage.getItem("accessToken");
-  if (storedAccessToken) {
-    return storedAccessToken;
+const getIdentityTokenFromStorageAndCleanUrl = (): string | null => {
+  const storedIdentityToken = localStorage.getItem("identityToken");
+  if (storedIdentityToken) {
+    return storedIdentityToken;
   }
   const queryParams = new URLSearchParams(window.location.search);
-  const accessTokenFromUrl = queryParams.get("access_token");
-  if (accessTokenFromUrl) {
-    localStorage.setItem("accessToken", accessTokenFromUrl);
-    return accessTokenFromUrl;
+  const identityTokenFromUrl = queryParams.get("access_token");
+  if (identityTokenFromUrl) {
+    localStorage.setItem("identityToken", identityTokenFromUrl);
+    return identityTokenFromUrl;
   }
 
   return null;
 };
 
 export const LoginOrDisplayUser = () => {
-  const accessToken = getAccessTokenFromStorageAndCleanUrl();
+  const identityToken = getIdentityTokenFromStorageAndCleanUrl();
 
   const disconnect = useCallback(() => {
-    localStorage.removeItem("accessToken");
+    localStorage.removeItem("identityToken");
     location.reload();
   }, []);
 
-  if (accessToken) {
+  if (identityToken) {
     const decodedToken =
-      decodeJWT<{ name: string; picture: string }, any>(accessToken);
+      decodeJWT<{ name: string; picture: string }, any>(identityToken);
     const name = decodedToken.payload.name;
     const pictureUrl = decodedToken.payload.picture;
     return <NavUser name={name} src={pictureUrl} onClick={disconnect} />;
